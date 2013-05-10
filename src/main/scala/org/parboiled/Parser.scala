507020b1a5df8fe2b5441421a5446cb78ca97663
Alexander Myltsev <alexander.myltsev@phystech.edu>
2013-05-10 18:01:38 +0400

    Move AST transformations to OpTree

--- src/main/scala/org/parboiled/Parser.scala
+++ src/main/scala/org/parboiled/Parser.scala
@@ -16,8 +16,6 @@
 
 package org.parboiled
 
-import scala.reflect.macros.Context
-
 import org.parboiled.optree._
 
 abstract class Parser(input: ParserInput) {
@@ -38,83 +36,8 @@ abstract class Parser(input: ParserInput) {
 }
 
 object Parser {
-  class GrammarException(_msg: String) extends RuntimeException(_msg)
-
-  type ParserContext = Context { type PrefixType = Parser }
-
-  def transformToOpTree(c: ParserContext)(e: c.Tree): Expression = {
-    import c.universe._
-
-    e match {
-      case Apply(Select(This(typeName), termName), List(Select(This(argTypeName), argTermName))) if argTermName.decoded == "EOI" ⇒
-        EOI
-
-      case Apply(Select(This(typeName), termName), List(Select(This(argTypeName), argTermName))) if termName.decoded == "stringRule" ⇒
-        fail("Strings in rule definitions have to be literals")
-
-      case Apply(Select(This(typeName), termName), List(Literal(Constant(x: String)))) ⇒
-        LiteralString(x)
-
-      case Apply(Select(This(typeName), termName), List(Literal(Constant(x: Char)))) ⇒
-        LiteralChar(x)
-
-      case Apply(Select(a, n), List(arg)) if show(n) == "newTermName(\"$tilde\")" ⇒
-        Sequence(transformToOpTree(c)(a), transformToOpTree(c)(arg))
-
-      case Apply(Select(a, n), List(arg)) if show(n) == "newTermName(\"$bar$bar\")" ⇒
-        FirstOf(transformToOpTree(c)(a), transformToOpTree(c)(arg))
-
-      case x ⇒
-        fail("transformToOpTree, unexpected: " + showRaw(x))
-    }
+  def ruleImpl(c: OpTree.ParserContext)(r: c.Expr[Rule]): c.Expr[Boolean] = {
+    val opTree = OpTree.fromAst(c)(r.tree)
+    OpTree.toAst(c)(opTree)
   }
-
-  def transformToAst(c: ParserContext)(e: Expression): c.Expr[Boolean] = {
-    import c.universe._
-
-    e match {
-      case EOI ⇒ reify {
-        val p = c.prefix.splice
-        p.nextChar == p.EOI
-      }
-
-      case LiteralString(targetString: String) ⇒ reify {
-        val p = c.prefix.splice
-        val cursorPos = p.cursor
-        val ts = c.literal(targetString).splice
-        if (ts forall (_ == p.nextChar)) true
-        else { p.cursor = cursorPos; false }
-      }
-
-      case LiteralChar(targetChar: Char) ⇒ reify {
-        val p = c.prefix.splice
-        val tc = c.literal(targetChar).splice
-        p.nextChar == tc
-      }
-
-      case Sequence(lhs: Expression, rhs: Expression) ⇒ reify {
-        val p = c.prefix.splice
-        val cursorPos = p.cursor
-        if (transformToAst(c)(lhs).splice) transformToAst(c)(rhs).splice
-        else { p.cursor = cursorPos; false }
-      }
-
-      case FirstOf(lhs: Expression, rhs: Expression) ⇒ reify {
-        val p = c.prefix.splice
-        val cursorPos = p.cursor
-        if (transformToAst(c)(lhs).splice) true
-        else { p.cursor = cursorPos; transformToAst(c)(rhs).splice }
-      }
-
-      case x ⇒
-        fail("transformToAst, unexpected: " + x)
-    }
-  }
-
-  def ruleImpl(c: ParserContext)(r: c.Expr[Rule]): c.Expr[Boolean] = {
-    val opTree = transformToOpTree(c)(r.tree)
-    transformToAst(c)(opTree)
-  }
-
-  private def fail(errorMsg: String) = throw new GrammarException(errorMsg)
 }
