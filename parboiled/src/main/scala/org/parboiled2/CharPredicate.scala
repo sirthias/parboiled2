3f8a42db51fb31ceba81e14b88b36b385796d1c2
Mathias <mathias@parboiled.org>
2013-12-11 14:36:51 +0100

    Add CharPredicate and CharUtils

---
+++ parboiled/src/main/scala/org/parboiled2/CharPredicate.scala
@@ -0,0 +1,243 @@
+/*
+ * Copyright (C) 2009-2013 Mathias Doenitz, Alexander Myltsev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.parboiled2
+
+import scala.annotation.tailrec
+
+sealed abstract class CharPredicate extends (Char ⇒ Boolean) {
+  import CharPredicate._
+
+  def ++(that: CharPredicate): CharPredicate
+  def ++(chars: Seq[Char]): CharPredicate
+  def --(that: CharPredicate): CharPredicate
+  def --(chars: Seq[Char]): CharPredicate
+
+  def ++(chars: Char): CharPredicate = this ++ (chars :: Nil)
+  def --(chars: Char): CharPredicate = this -- (chars :: Nil)
+  def ++(chars: String): CharPredicate = this ++ chars.toCharArray
+  def --(chars: String): CharPredicate = this -- chars.toCharArray
+
+  def intersect(that: CharPredicate): CharPredicate
+
+  def unary_!(): CharPredicate = this match {
+    case Empty ⇒ All
+    case All   ⇒ Empty
+    case _     ⇒ from(c ⇒ !this(c))
+  }
+
+  def matchAny(string: String): Boolean = {
+    @tailrec def rec(ix: Int): Boolean =
+      if (ix == string.length) false else if (this(string charAt ix)) true else rec(ix + 1)
+    rec(0)
+  }
+
+  def matchAll(string: String): Boolean = {
+    @tailrec def rec(ix: Int): Boolean =
+      if (ix == string.length) true else if (!this(string charAt ix)) false else rec(ix + 1)
+    rec(0)
+  }
+
+  def indexOfFirstMatch(string: String): Int = {
+    @tailrec def rec(ix: Int): Int =
+      if (ix == string.length) -1 else if (this(string charAt ix)) ix else rec(ix + 1)
+    rec(0)
+  }
+
+  def indexOfFirstMismatch(string: String): Int = {
+    @tailrec def rec(ix: Int): Int =
+      if (ix == string.length) -1 else if (this(string charAt ix)) rec(ix + 1) else ix
+    rec(0)
+  }
+
+  def firstMatch(string: String): Option[Char] =
+    indexOfFirstMatch(string) match {
+      case -1 ⇒ None
+      case ix ⇒ Some(string charAt ix)
+    }
+
+  def firstMismatch(string: String): Option[Char] =
+    indexOfFirstMismatch(string) match {
+      case -1 ⇒ None
+      case ix ⇒ Some(string charAt ix)
+    }
+
+  private def or(that: Char ⇒ Boolean): CharPredicate =
+    if (this == Empty) CharPredicate(that) else from(c ⇒ this(c) || that(c))
+  private def and(that: Char ⇒ Boolean) =
+    if (this == Empty) Empty else from(c ⇒ this(c) && that(c))
+  private def andNot(that: Char ⇒ Boolean) =
+    if (this == Empty) from(!that(_)) else from(c ⇒ this(c) && !that(c))
+}
+
+object CharPredicate {
+  val Empty: CharPredicate = CharMask(0L, 0L)
+  val All: CharPredicate = from(_ ⇒ true)
+  val LowerAlpha = CharPredicate('a' to 'z')
+  val UpperAlpha = CharPredicate('A' to 'Z')
+  val Alpha = LowerAlpha ++ UpperAlpha
+  val Digit = CharPredicate('0' to '9')
+  val AlphaNum = Alpha ++ Digit
+  val LowerHexLetter = CharPredicate('a' to 'f')
+  val UpperHexLetter = CharPredicate('A' to 'F')
+  val HexLetter = LowerHexLetter ++ UpperHexLetter
+  val HexAlpha = Digit ++ HexLetter
+  val Visible = CharPredicate('\u0021' to '\u007e')
+  val Printable = Visible ++ ' '
+
+  def from(predicate: Char ⇒ Boolean): CharPredicate =
+    predicate match {
+      case x: CharPredicate ⇒ x
+      case x                ⇒ GeneralCharPredicate(x)
+    }
+
+  def apply(magnets: ApplyMagnet*): CharPredicate = (Empty /: magnets) { (a, m) ⇒ a ++ m.predicate }
+
+  class ApplyMagnet(val predicate: CharPredicate)
+  object ApplyMagnet {
+    implicit def fromPredicate(predicate: Char ⇒ Boolean): ApplyMagnet = new ApplyMagnet(from(predicate))
+    implicit def fromChar(c: Char): ApplyMagnet = fromChars(c :: Nil)
+    implicit def fromString(chars: String): ApplyMagnet = fromChars(chars)
+    implicit def fromChars(chars: Seq[Char]): ApplyMagnet = {
+      @tailrec def rec(ix: Int, result: CharPredicate): CharPredicate =
+        if (ix == chars.length) result else rec(ix + 1, result ++ chars(ix))
+      new ApplyMagnet(rec(0, Empty))
+    }
+  }
+
+  ///////////////////////// PRIVATE ////////////////////////////
+
+  private def ranged(c: Char) = c & ((c - 128) >> 31) // branchless for `if (c < 128) c else 0`
+  private def maskable(c: Char) = '\u0000' < c && c < '\u0080'
+
+  // efficient handling of 7bit-ASCII chars excluding 0x00
+  case class CharMask private[CharPredicate] (lowMask: Long, highMask: Long) extends CharPredicate {
+    def apply(char: Char): Boolean = {
+      val c = ranged(char)
+      if (c < 64) (lowMask & (1L << c)) != 0L
+      else (highMask & (1L << (c - 64))) != 0L
+    }
+
+    def ++(that: CharPredicate): CharPredicate = that match {
+      case Empty               ⇒ this
+      case _ if this == Empty  ⇒ that
+      case CharMask(low, high) ⇒ CharMask(lowMask | low, highMask | high)
+      case _                   ⇒ this or that
+    }
+
+    def ++(chars: Seq[Char]): CharPredicate = chars.foldLeft(this: CharPredicate) {
+      case (_: CharMask, c) if !maskable(c)   ⇒ this or new ArrayBasedPredicate(chars.toArray)
+      case (CharMask(low, high), c) if c < 64 ⇒ CharMask(low | 1L << c, high)
+      case (CharMask(low, high), c)           ⇒ CharMask(low, high | 1L << (c - 64))
+      case (x, _)                             ⇒ x // once the fold acc is not a CharMask we are done
+    }
+
+    def --(that: CharPredicate): CharPredicate = that match {
+      case Empty               ⇒ this
+      case _ if this == Empty  ⇒ this
+      case CharMask(low, high) ⇒ CharMask(lowMask & ~low, highMask & ~high)
+      case _                   ⇒ this andNot that
+    }
+
+    def --(chars: Seq[Char]): CharPredicate =
+      if (this != Empty) {
+        chars.foldLeft(this: CharPredicate) {
+          case (_: CharMask, c) if !maskable(c)   ⇒ this andNot new ArrayBasedPredicate(chars.toArray)
+          case (CharMask(low, high), c) if c < 64 ⇒ CharMask(low & ~(1L << c), high)
+          case (CharMask(low, high), c)           ⇒ CharMask(low, high & ~(1L << (c - 64)))
+          case (x, _)                             ⇒ x // once the fold acc is not a CharMask we are done
+        }
+      } else this
+
+    def intersect(that: CharPredicate) = that match {
+      case Empty               ⇒ Empty
+      case _ if this == Empty  ⇒ Empty
+      case CharMask(low, high) ⇒ CharMask(lowMask & low, highMask & high)
+      case _                   ⇒ this and that
+    }
+
+    override def toString(): String = "CharMask(%016x|%016x)" format (lowMask, highMask)
+  }
+
+  class ArrayBasedPredicate private[CharPredicate] (private val chars: Array[Char]) extends CharPredicate {
+    import java.util.Arrays._
+    sort(chars)
+
+    def apply(c: Char): Boolean = binarySearch(chars, c) > 0
+
+    def ++(that: CharPredicate) = that match {
+      case Empty                  ⇒ this
+      case x: ArrayBasedPredicate ⇒ this ++ x.chars
+      case _                      ⇒ this or that
+    }
+
+    def ++(other: Seq[Char]) =
+      if (other.nonEmpty) new ArrayBasedPredicate((this -- other).chars ++ other.toArray[Char])
+      else this
+
+    def --(that: CharPredicate) = that match {
+      case Empty                  ⇒ this
+      case x: ArrayBasedPredicate ⇒ this -- x.chars
+      case _                      ⇒ this andNot that
+    }
+
+    def --(other: Seq[Char]) =
+      if (other.nonEmpty) {
+        val otherChars = other.toArray
+        new ArrayBasedPredicate(chars.filter(binarySearch(otherChars, _) < 0))
+      } else this
+
+    def intersect(that: CharPredicate) = that match {
+      case Empty                  ⇒ Empty
+      case x: ArrayBasedPredicate ⇒ new ArrayBasedPredicate(chars.intersect(x.chars))
+      case _                      ⇒ this and that
+    }
+  }
+
+  case class GeneralCharPredicate private[CharPredicate] (predicate: Char ⇒ Boolean) extends CharPredicate {
+    def apply(c: Char) = predicate(c)
+
+    def ++(that: CharPredicate): CharPredicate = that match {
+      case Empty                               ⇒ this
+      case GeneralCharPredicate(thatPredicate) ⇒ from(c ⇒ predicate(c) || thatPredicate(c))
+      case _                                   ⇒ from(c ⇒ predicate(c) || that(c))
+    }
+
+    def ++(chars: Seq[Char]): CharPredicate =
+      if (chars.nonEmpty) {
+        val abp = new ArrayBasedPredicate(chars.toArray)
+        from(c ⇒ predicate(c) || abp(c))
+      } else this
+
+    def --(that: CharPredicate): CharPredicate = that match {
+      case Empty                               ⇒ this
+      case GeneralCharPredicate(thatPredicate) ⇒ from(c ⇒ predicate(c) && !thatPredicate(c))
+      case _                                   ⇒ from(c ⇒ predicate(c) && !that(c))
+    }
+
+    def --(chars: Seq[Char]): CharPredicate =
+      if (chars.nonEmpty) {
+        val abp = new ArrayBasedPredicate(chars.toArray)
+        from(c ⇒ predicate(c) && !abp(c))
+      } else this
+
+    def intersect(that: CharPredicate) = that match {
+      case Empty                               ⇒ Empty
+      case GeneralCharPredicate(thatPredicate) ⇒ from(c ⇒ predicate(c) && that(c))
+      case _                                   ⇒ this and that
+    }
+  }
+}
---
+++ parboiled/src/main/scala/org/parboiled2/CharUtils.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2009-2013 Mathias Doenitz, Alexander Myltsev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.parboiled2
+
+object CharUtils {
+  /**
+   * Returns the int value of a given hex digit char.
+   * Note: this implementation is very fast (since it's branchless) and therefore
+   * does not perform ANY range checks!
+   */
+  def hexValue(c: Char): Int = (c & 0x1f) + ((c >> 6) * 0x19) - 0x10
+
+  /**
+   * Returns the lower-case hex digit corresponding to the last 4 bits of the given Long.
+   * (fast branchless implementation)
+   */
+  def lowerHexDigit(long: Long): Char = lowerHexDigit_internal((long & 0x0FL).toInt)
+
+  /**
+   * Returns the lower-case hex digit corresponding to the last 4 bits of the given Int.
+   * (fast branchless implementation)
+   */
+  def lowerHexDigit(int: Int): Char = lowerHexDigit_internal(int & 0x0F)
+
+  private def lowerHexDigit_internal(i: Int) = (48 + i + (39 & ((9 - i) >> 31))).toChar
+
+  /**
+   * Returns the upper-case hex digit corresponding to the last 4 bits of the given Long.
+   * (fast branchless implementation)
+   */
+  def upperHexDigit(long: Long): Char = upperHexDigit_internal((long & 0x0FL).toInt)
+
+  /**
+   * Returns the upper-case hex digit corresponding to the last 4 bits of the given Int.
+   * (fast branchless implementation)
+   */
+  def upperHexDigit(int: Int): Char = upperHexDigit_internal(int & 0x0F)
+
+  private def upperHexDigit_internal(i: Int) = (48 + i + (7 & ((9 - i) >> 31))).toChar
+
+  /**
+   * Efficiently lower-cases the given character.
+   * Note: only works for 7-bit ASCII letters.
+   */
+  def toLowerCase(c: Char): Char = if (CharPredicate.UpperAlpha(c)) (c + 0x20).toChar else c
+
+  /**
+   * Efficiently upper-cases the given character.
+   * Note: only works for 7-bit ASCII letters.
+   */
+  def toUpperCase(c: Char): Char = if (CharPredicate.LowerAlpha(c)) (c + 0x20).toChar else c
+
+  def escape(c: Char): String = c match {
+    case '\t'                           ⇒ "\\t"
+    case '\r'                           ⇒ "\\r"
+    case '\n'                           ⇒ "\\n"
+    case x if Character.isISOControl(x) ⇒ "\\u%04x" format c.toInt
+    case x                              ⇒ x.toString
+  }
+}
